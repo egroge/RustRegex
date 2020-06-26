48e6b912f4167af89a0152b48402af40f7f19916
George Stacey <gs919@ic.ac.uk>
2020-06-26 18:27:08 +0100

    Tidied lexer and added new tests. Still not especially happy with the layout but seems to work

--- src/lib.rs
+++ src/lib.rs
@@ -8,22 +8,32 @@ enum Lexeme {
     LRound,
     RRound,
 }
+
 use Lexeme::*;
-// Assumes well formed
-fn lex_class(s: &str) -> (String, Vec<Lexeme>) {
+fn lex_class(s: &str) -> (u32, Vec<Lexeme>) {
     let mut backslash = false;
     let mut lexed: Vec<Lexeme> = vec![];
     let mut i = 0;
 
     for c in s.chars() {
+        if backslash {
+            if "wbdsWBDS".contains(c) {
+                lexed.push(Meta(c));
+            } else {
+                lexed.push(Ch(c));
+            }
+            i += 1;
+            backslash = false;
+            continue;
+        }
+        assert!(!backslash);
+
         match c {
-            ']' => return (s[i..].to_string(), lexed),
+            ']' => return (i, lexed),
             '\\' => {
-                if !backslash {
-                    i += 1;
-                    backslash = true;
-                    continue;
-                }
+                i += 1;
+                backslash = true;
+                continue;
             }
             _ => (),
         }
@@ -31,20 +41,7 @@ fn lex_class(s: &str) -> (String, Vec<Lexeme>) {
         i += 1;
 
         let lexeme = match c {
-            'w' | 'W' | 'b' | 'B' | 'd' | 'D' | 's' | 'S' => {
-                if backslash {
-                    Meta(c)
-                } else {
-                    Ch(c)
-                }
-            }
-            '-' | '^' => {
-                if backslash {
-                    Ch(c)
-                } else {
-                    Op(c)
-                }
-            }
+            '-' | '^' => Op(c),
             _ => Ch(c),
         };
 
@@ -52,66 +49,76 @@ fn lex_class(s: &str) -> (String, Vec<Lexeme>) {
         backslash = false;
     }
 
-    (s[i..].to_string(), lexed)
+    (i, lexed)
 }
 
-fn lex(s: &str) -> Vec<Lexeme> {
-    if s == "" {
-        return vec![];
-    }
+// If we have a backslash, whatever comes after is a meta, or a ch
+
+fn lex(s: &str) -> Result<Vec<Lexeme>, &'static str> {
     let mut lexed: Vec<Lexeme> = vec![];
-    let mut i = 0;
     let mut backslash = false;
-    for c in s.chars() {
-        if !backslash && c == '[' {
-            lexed.push(LSquare);
-            let (remaining, subclass) = lex_class(&s[i + 1..]);
-            lexed.extend(subclass);
-            lexed.extend(lex(remaining.as_str()));
-            return lexed;
+
+    let mut num_round = 0;
+    let mut num_square = 0;
+
+    let s: Vec<char> = s.chars().collect();
+
+    let mut i = 0;
+    while i < s.len() {
+        let c = s[i];
+        i += 1;
+        if backslash {
+            if "wbdsWBDS".contains(c) {
+                lexed.push(Meta(c));
+            } else {
+                lexed.push(Ch(c));
+            }
+            backslash = false;
+            continue;
         }
 
-        i += 1;
-        if !backslash && c == '\\' {
+        assert!(!backslash);
+
+        if c == '[' {
+            lexed.push(LSquare);
+            num_square += 1;
+            let (skipped, subclass) = lex_class(s[i..].iter().collect::<String>().as_str());
+            lexed.extend(subclass);
+            i += skipped as usize;
+            continue;
+        }
+
+        if c == '\\' {
             backslash = true;
             continue;
         }
 
         let lexeme = match c {
-            'w' | 'W' | 'b' | 'B' | 'd' | 'D' | 's' | 'S' => {
-                if backslash {
-                    Meta(c)
-                } else {
-                    Ch(c)
-                }
+            '(' => {
+                num_round += 1;
+                LRound
             }
-            '(' => LRound,
-            ')' => RRound,
-            // TODO this is bad, but should work because already checked
-            // for backslash at start of loop
-            '[' => Ch('['),
-            ']' => RSquare,
-            '+' | '?' | '*' => {
-                if backslash {
-                    Ch(c)
-                } else {
-                    Op(c)
-                }
+            ')' => {
+                num_round -= 1;
+                RRound
             }
-            '.' => {
-                if backslash {
-                    Ch(c)
-                } else {
-                    Meta(c)
-                }
+            ']' => {
+                num_square -= 1;
+                RSquare
             }
+            '+' | '?' | '*' => Op(c),
+            '.' => Meta(c),
             _ => Ch(c),
         };
 
         lexed.push(lexeme);
         backslash = false;
     }
-    lexed
+    if num_round != 0 || num_square != 0 {
+        Err("Mismatched brackets")
+    } else {
+        Ok(lexed)
+    }
 }
 
 #[cfg(test)]
@@ -123,23 +130,23 @@ mod tests {
         #[test]
         fn lex_class_test() {
             let (remains, lexed) = lex_class("A-Za-z");
-            assert_eq!(remains, "");
+            assert_eq!(remains, 6);
             assert_eq!(
                 lexed,
                 vec![Ch('A'), Op('-'), Ch('Z'), Ch('a'), Op('-'), Ch('z')]
             );
 
             let (remains, lexed) = lex_class("A-Z_]world");
-            assert_eq!(remains, "]world");
+            assert_eq!(remains, 4);
             assert_eq!(lexed, vec![Ch('A'), Op('-'), Ch('Z'), Ch('_')]);
         }
 
         #[test]
         fn lex_test() {
-            let lexed = lex("yee+t");
+            let lexed = lex("yee+t").expect("Problem lexing");
             assert_eq!(lexed, vec![Ch('y'), Ch('e'), Ch('e'), Op('+'), Ch('t')]);
 
-            let lexed = lex("g[A-Z+.]+.");
+            let lexed = lex("g[A-Z+.]+.").expect("Problem lexing");
             assert_eq!(
                 lexed,
                 vec![
@@ -156,7 +163,7 @@ mod tests {
                 ]
             );
 
-            let lexed = lex(r"h?m+\.[\w]\?[\d]?");
+            let lexed = lex(r"h?m+\.[\w]\?[\d]?").expect("Problem lexing");
             assert_eq!(
                 lexed,
                 vec![
@@ -176,7 +183,7 @@ mod tests {
                 ]
             );
 
-            let lexed = lex("(a)?b(c)*");
+            let lexed = lex("(a)?b(c)*").expect("Problem lexing");
             assert_eq!(
                 lexed,
                 vec![
@@ -193,10 +200,16 @@ mod tests {
             );
 
             let lexed = lex("h[");
-            assert_eq!(lexed, vec![Ch('h'), LSquare]);
+            assert!(lexed.is_err());
 
-            let lexed = lex(r"h\\h\w");
+            let lexed = lex(r"h\\h\w").expect("Problem lexing");
             assert_eq!(lexed, vec![Ch('h'), Ch('\\'), Ch('h'), Meta('w')]);
+
+            let lexed = lex(r"[[\]]").expect("Problem lexing");
+            assert_eq!(lexed, vec![LSquare, Ch('['), Ch(']'), RSquare]);
+
+            let lexed = lex(r"([]))");
+            assert!(lexed.is_err());
         }
     }
 }
